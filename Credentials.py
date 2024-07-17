f0f16d2c6a34b0c09de9395d10512b774c5b7c94
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-07-18 02:12:23 +0300

    Feat: Callable variables for credentials update started

--- Credentials.py
+++ Credentials.py
@@ -5,7 +5,14 @@ class Credentials:
     def __init__(self):
         # adding an operation passwd, applicable in deleting passwords.
         self.credentials = {}
-        
+
+
+    # Edit credentials by overriding the __call__ method for the credential fields.
+    def __call__(self, account, user_name, password):
+        self.account = account
+        self.user_name = user_name
+        self.password = password
+
     # creating new credentials and storing them in a list.
     def set_credentials(self, account,user_name, password):
         # creation
@@ -25,11 +32,16 @@ class Credentials:
             for account in self.credentials:
                 if account_name == account:
                     if account is not None:
-                        return f'The credentials associated with the \'{account_name}\' account are:\nUser name: {self.credentials[account]['username']}\nPassword: {self.credentials[account]['password']}\n'
+                        return f'The credentials associated with the \'{account_name}\' account are:\naccount: {account_name}\nuser_name: {self.credentials[account]['username']}\nPassword: {self.credentials[account]['password']}\n\n'
                     else:
                         pass
         except Exception as e:
             print(f'Oops, no credentials found for {account_name}', e)
+    
+
+    # Read passwords from file.
+    # decide the format to read and how to store it for later retrieval.
+
 
 
 # Account = Credentials()
@@ -52,16 +64,17 @@ def main():
     print('Enter option 5 to Exit the system.')
     Account = Credentials()
     
-    # 
+    # exporter function.
     def exporter(account_name):
     # this function needs to append retrieved credentials from user account search.
         with open('exported_passwords.txt', 'a+') as exported_passwords:
             exported_passwords.write(Account.export_passwords(account_name))
     
     while True:
+        print('---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----')
         print('Select an option you wish to continue with.\n')
         # creating user options and displaying them to the user.
-        options = {1:'Save a password', 2:'Show saved password by account name', 3:'Delete saved password', 4:'Export a saved passwords',5:'Exit password manager'}
+        options = {1:'Save a password', 2:'Show saved password by account name', 3:'Delete saved password', 4:'Export a saved passwords', 5:'Update a saved passwords',6:'Exit password manager'}
         
         for option in options:
             print(f'{option}: {options[option]}')
@@ -118,11 +131,23 @@ def main():
                     except Exception as e:
                         print('An exception occurred while exporting passwords, try again later.\n')
                         print(f'{e}\n')
-                        
+                
+                elif user_input == 5:
+                    print('\nwhich account do you want to update saved passwords?\n')
+                    try:
+                        user_input_account = input('Enter account name: ')
+                        if user_input_account is not None:
+                            ...
+                        else:
+                            pass
+                    except Exception as e:
+                        print('An exception occurred while exporting passwords, try again later.\n')
+                        print(f'{e}\n')
+                
                 
                 
                 # fifth option logic, exiting the password manager.
-                elif user_input == 5:
+                elif user_input == 6:
                     print('\nExiting the password manager...')
                     print('Goodbye 😀\n')
                     break
